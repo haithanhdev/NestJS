f486ebff6851e73376e4c5ba22fe437d41d86539
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-03-23 19:13:43 +0700

    Setup Prisma with SQLite and validate .env file

---
+++ src/routes/posts/posts.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common'
+import { PrismaService } from 'src/shared/services/prisma.service'
+
+@Injectable()
+export class PostsService {
+  constructor(private readonly prismaService: PrismaService) {}
+  getPosts() {
+    return this.prismaService.post.findMany({})
+  }
+  getPost(id: string) {
+    return `Post ${id}`
+  }
+  createPost(body: any) {
+    const userId = 1
+    return this.prismaService.post.create({
+      data: {
+        title: body.title,
+        content: body.content,
+        authorId: userId,
+      },
+    })
+  }
+  updatePost(id: string, body: any) {
+    return `Update post ${id} with body ${body}`
+  }
+  deletePost(id: string) {
+    return `Delete post ${id}`
+  }
+}
---
+++ src/shared/config.ts
@@ -0,0 +1,49 @@
+import { plainToInstance } from 'class-transformer'
+import { IsNumber, IsString, validateSync } from 'class-validator'
+import fs from 'fs'
+import path from 'path'
+import { config } from 'dotenv'
+config({
+  path: path.resolve('.env'),
+})
+//Kiểm tra xem đã có file .env hay chưa?
+
+if (!fs.existsSync(path.resolve('.env'))) {
+  console.log('File .env not exist')
+  //Ngừng ứng dụng
+  process.exit(1)
+}
+
+class ConfigSchema {
+  @IsString()
+  DATABASE_URL: string
+  @IsNumber()
+  PORT: number
+  @IsString()
+  ACCESS_TOKEN_SECRET: string
+  @IsString()
+  ACCESS_TOKEN_EXPIRES_IN: string
+  @IsString()
+  REFRESH_TOKEN_SECRET: string
+  @IsString()
+  REFRESH_TOKEN_EXPIRES_IN: string
+}
+console.log(process.env)
+const configServer = plainToInstance(ConfigSchema, process.env, {
+  enableImplicitConversion: true,
+})
+const e = validateSync(configServer)
+if (e.length > 0) {
+  console.log('Các giá trị khai báo trong file .env không hợp lệ')
+  const errors = e.map((eitem) => {
+    return {
+      property: eitem.property,
+      constraints: eitem.constraints,
+      value: eitem.value,
+    }
+  })
+  throw errors
+}
+// console.log(configServer)
+const envConfig = configServer
+export default envConfig
