13ac6d530233fe3bca6303880b52e9adaee2ca76
Nguyen Hai Thanh <edu.haithanh.nguyen1001@gmail.com>
2025-03-24 09:43:17 +0700

    Base register user

---
+++ src/routes/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { ConflictException, Injectable } from '@nestjs/common'
+import { Prisma } from '@prisma/client'
+import { HashingService } from 'src/shared/services/hashing.service'
+import { PrismaService } from 'src/shared/services/prisma.service'
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly hashingService: HashingService,
+    private readonly prismaService: PrismaService,
+  ) {}
+  async register(body: any) {
+    try {
+      const hashedPassword = await this.hashingService.hash(body.password)
+      const user = await this.prismaService.user.create({
+        data: {
+          email: body.email,
+          name: body.name,
+          password: hashedPassword,
+        },
+      })
+      return user
+    } catch (error) {
+      console.log(error)
+      if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2002') {
+        throw new ConflictException('Email already exists')
+      }
+      throw error
+    }
+  }
+}
--- src/shared/config.ts
+++ src/shared/config.ts
@@ -7,7 +7,6 @@ config({
   path: path.resolve('.env'),
 })
 //Kiểm tra xem đã có file .env hay chưa?
-
 if (!fs.existsSync(path.resolve('.env'))) {
   console.log('File .env not exist')
   //Ngừng ứng dụng
@@ -32,10 +31,10 @@ console.log(process.env)
 const configServer = plainToInstance(ConfigSchema, process.env, {
   enableImplicitConversion: true,
 })
-const e = validateSync(configServer)
-if (e.length > 0) {
+const errorArray = validateSync(configServer)
+if (errorArray.length > 0) {
   console.log('Các giá trị khai báo trong file .env không hợp lệ')
-  const errors = e.map((eitem) => {
+  const errors = errorArray.map((eitem) => {
     return {
       property: eitem.property,
       constraints: eitem.constraints,
